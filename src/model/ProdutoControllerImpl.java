c1f1feaa098cf34c3a2331e9f9666d6104d76699
ercosta1993 <edson.costa@acad.pucrs.br>
2023-06-22 22:59:39 -0300

    Controller modificada

--- src/model/ProdutoControllerImpl.java
+++ src/model/ProdutoControllerImpl.java
@@ -6,7 +6,24 @@ import java.util.ArrayList;
 public class ProdutoControllerImpl implements ProdutoController {
 
     @Override
-    public Produto buscaProdutoPorID(int id, ArrayList<Produto> produtos) {
+    public void cadastrarProduto(Produto produto, ArrayList<Produto> produtos) {
+        if (buscarProdutoPorID(produto.getId(), produtos) == null) {
+            produtos.add(produto);
+        }
+    }
+
+    @Override
+    public void alterarProduto(int index, ArrayList<Produto> produtos) {
+        produtos.set(index, produtos.get(index));
+    }
+
+    @Override
+    public void removerProduto(int index, ArrayList<Produto> produtos) {
+        produtos.remove(index);
+    }
+
+    @Override
+    public Produto buscarProdutoPorID(int id, ArrayList<Produto> produtos) {
         for (int i = 0; i < produtos.size(); i++) {
             if (produtos.get(i).getId() == id) {
                 return produtos.get(i);
@@ -14,27 +31,33 @@ public class ProdutoControllerImpl implements ProdutoController {
         }
         return null;
     }
-
+    
     @Override
-    public void cadastrarProduto(Produto produto, ArrayList<Produto> produtos) {
-        if (buscaProdutoPorID(produto.getId(), produtos) == null) {
-            produtos.add(produto);
-        }
+    public ArrayList<Produto> filtrarProdutosPorDescricao(String nome, ArrayList<Produto> produtos) {
+        ArrayList<Produto> encontrados = new ArrayList<>();
+        produtos.stream().filter((p) -> (p.getDescricao().contains(nome))).forEachOrdered((p) -> {
+            encontrados.add(p);
+        });
+        return encontrados;
     }
 
     @Override
-    public void alterarProduto(Produto produto, ArrayList<Produto> produtos) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ArrayList<Produto> filtrarProdutosPorValorUnitarioMenorIgual(double valorUnitario, ArrayList<Produto> produtos) {
+        ArrayList<Produto> encontrados = new ArrayList<>();
+        produtos.stream().filter((p) -> (p.getValor_unitario() <= valorUnitario)).forEachOrdered((p) -> {
+            encontrados.add(p);
+        });
+        return encontrados;
     }
 
     @Override
-    public void removerProduto(Produto produto, ArrayList<Produto> produtos) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ArrayList<Produto> filtrarProdutosPorValorUnitarioMaiorIgual(double valorUnitario, ArrayList<Produto> produtos) {
+        ArrayList<Produto> encontrados = new ArrayList<>();
+        produtos.stream().filter((p) -> (p.getValor_unitario() >= valorUnitario)).forEachOrdered((p) -> {
+            encontrados.add(p);
+        });
+        return encontrados;
     }
-
-    @Override
-    public String exibirProduto(Produto produto, ArrayList<Produto> produtos) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+    
+    
 }
